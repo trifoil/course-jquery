1c647e865fa8abc77179774750c49c6cd3f9822f
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-08-23 22:13:29 +0200

    exo ok

--- resolus/Examen juin 2022 Questionnaire 1/script.js
+++ resolus/Examen juin 2022 Questionnaire 1/script.js
@@ -1,3 +1,29 @@
 $(function(){
-    console.log("nigga");
+    let total = 0;
+    $('.bouton').each(function(){   // cycle through all .bouton elements
+        let valeatoire = Math.floor(Math.random()*3+1);
+        if ($(this).find('.prix').hasClass('cher')){  //on cherche ce qui est a l'intérieur, avec la classe prix, et on check si il a la classe cher.
+            valeatoire+=3;
+        }
+        $(this).find('.prix span').text(valeatoire);
+
+
+    })
+    $(".bouton").click(function(){
+        console.log($(this).find(".nom").html());
+        $("#main").append('<div class="article"><img src="png/'+$(this).find(".nom").text()+'.png" style="width: 32px;" alt="" ></div>');
+    
+        console.log($(this).find(".prix").text());
+        
+        total += parseInt($(this).find(".prix").text().slice(0,-2))
+        console.log(total)
+        $('footer').text(total+' €')
+    })
+
+    $('.boutonOK').click(function(){
+        console.log("ntm")
+        $('footer').addClass("off")
+        $(".bouton").off('click')
+    })
+
 })
