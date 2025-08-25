478b2b7116426890c70c448848a6f17b8af3c0d1
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-08-25 14:39:43 +0200

    finito

--- resolus/Examen juin 2025/script.js
+++ resolus/Examen juin 2025/script.js
@@ -1,36 +1,81 @@
-$(function(){
+$(function () {
     console.log("nigga")
+    $("#save").attr("disabled", "disabled");
 
     let stars = 0;
+    let pseudo = $('#pseudo').val();
+    let commentaire = $('#commentaire').val();
+    console.log("pseudo : " + pseudo)
+    console.log("commentaire : " + commentaire)
 
-    $(".star").click(function(){
-        
+
+    $(".star").click(function () {
+
         stars = $(this).attr("data-value");
         //console.log(stars);
 
-        for (i=0; i<stars; i++){
+        for (i = 0; i < stars; i++) {
             // on trouve l'enfant de la classe stars qui a la data-value == i, puis on lui donne la classe ok
-            let j = parseInt(i)+1;
-            $(".stars").find('[data-value="'+j+'"]').addClass("ok");
+            let j = parseInt(i) + 1;
+            $(".stars").find('[data-value="' + j + '"]').addClass("ok");
         }
-        for (i=stars;i<5;i++){
-            let j = parseInt(i)+1;
+        for (i = stars; i < 5; i++) {
+            let j = parseInt(i) + 1;
             console.log(j);
-            $(".stars").find('[data-value="'+j+'"]').removeClass("ok");
+            $(".stars").find('[data-value="' + j + '"]').removeClass("ok");
+        }
+
+        if (stars != 0 && pseudo != "" && commentaire != "") {
+            $("#save").removeAttr("disabled");
+            //$("#save").attr()
+        }
+        else {
+            $("#save").attr("disabled", "disabled");
         }
     })
 
-    if(1){
-        $("#save").removeAttr("disabled");
-        //$("#save").attr()
-    }
+    $('#pseudo').on('input', function () {
+        pseudo = $('#pseudo').val()
+        console.log("valeur : " + pseudo)
 
-    $("#save").click(function(){
-        
+        if (stars != 0 && pseudo != "" && commentaire != "") {
+            $("#save").removeAttr("disabled");
+            //$("#save").attr()
+        }
+        else {
+            $("#save").attr("disabled", "disabled");
+        }
+
+    })
+
+    $("#commentaire").on('input', function () {
+        commentaire = $(this).val()
+        console.log("commentaire : " + commentaire)
+
+        if (stars != 0 && pseudo != "" && commentaire != "") {
+            $("#save").removeAttr("disabled");
+            //$("#save").attr()
+        }
+        else {
+            $("#save").attr("disabled", "disabled");
+        }
+    })
+
+
+
+
+    $("#save").click(function () {
+        $("#comments-container").prepend("<div class=\"comment\"><div class=\"comment-header\"><span class=\"pseudo\">" + pseudo + "</span><span class=\"note\">" + stars + "/5</span></div><p>" + commentaire + "</p></div>")
+        stars = 0;
+        pseudo = "";
+        commentaire = "";
+        $("#pseudo").val("");
+        $("#commentaire").val("");
+        $(".star").removeClass("ok");
         console.log("saved");
     })
 
-    $(".comment").click(function(){
+    $(".comment").click(function () {
         $(this).remove();
     })
 
@@ -38,3 +83,4 @@ $(function(){
 
 
 // attr() ou removeAttr()
+// .attr("disabled", "disabled") ou .prop("disabled", true).
